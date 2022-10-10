f286ce590bfd9947ea60ea5b61077fde7092f3f3
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-10-10 13:14:29 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Phonebook from './Phonebook/Phonebook';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onAddContact = (name, phone) => e => {
+    const id = nanoid();
+    const newContact = {
+      name,
+      id,
+      phone,
+    };
+    console.log(this.state);
+    if (
+      this.state.contacts.some(item => {
+        return item.name.toLowerCase() === name.toLowerCase();
+      })
+    ) {
+      alert(`${name} is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  };
+
+  handleChange = e => {
+    this.setState(() => ({
+      filter: e.target.value,
+    }));
+  };
+
+  onDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(item => item.id !== id),
+    }));
+  };
+
+  onFilteredArray = () => {
+    let filteredArray = [];
+    if (this.state.filter === '') {
+      filteredArray = [...this.state.contacts];
+      console.log(filteredArray);
+    } else if (this.state.filter !== '') {
+      this.state.contacts.map(item => {
+        if (item.name.toLowerCase().includes(this.state.filter.toLowerCase())) {
+          filteredArray.push(item);
+        }
+        return item;
+      });
+    }
+    return filteredArray;
+  };
+
+  render() {
+    return (
+      <>
+        <h2 className={css.header}>Phonebook</h2>
+        <Phonebook onAddContact={this.onAddContact} />
+        <h2 className={css.header}>Contacts</h2>
+        <Filter
+          filter={this.state.filter}
+          handleChange={this.handleChange}
+        ></Filter>
+        <Contacts
+          filteredArray={this.onFilteredArray}
+          onDelete={this.onDelete}
+        />
+      </>
+    );
+  }
+}
